d14bf06100298c77221cbc0b5204a8fdb7be777a
David Scheutz <scheutz.david@gmail.com>
2022-01-31 09:58:25 +0100

    initial commit

---
+++ github-demo/github-demoUITests/github_demoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  github_demoUITestsLaunchTests.swift
+//  github-demoUITests
+//
+//  Created by David's MBP16 on 31.01.22.
+//
+
+import XCTest
+
+class github_demoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
