87dadfbbbc558bf516c3f8fb28efccd67ef836f6
David Scheutz <scheutz.david@gmail.com>
2022-01-31 09:59:40 +0100

    add api tests

--- github-demo/github-demoTests/API_Tests.swift
+++ github-demo/github-demoTests/API_Tests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import github_demo
 
-class github_demoTests: XCTestCase {
+class API_Tests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
